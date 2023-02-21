1f4c0727b967d9cfdb2e22352e274abd9d3b20d7
max.kulbako <mkulbako@gmail.com>
2023-02-21 21:51:59 +0100

    add start page

--- src/views/components/Header.js
+++ src/views/components/Header.js
@@ -1,14 +1,24 @@
 import { NavBar } from "./NavBar";
 import { MobileNavBar } from "./MobileNavBar";
-import { LOGO_BLACK, navMenuList } from "../../core/constants/storage"
+import { LOGO_BLACK, navMenuList } from "../../core/constants/storage";
+import { StartPage } from "./StartPage";
+import React, {useState} from "react";
 
 
 export function Header () {
+  const [isStartOpen, setOpenStart] = useState(true);
+
+  const handleStartPage = () => {
+    setOpenStart(!isStartOpen);
+  }
+
   return (
-    <div className="header_wrapper">
-      <NavBar navList={navMenuList} logo={LOGO_BLACK}/>
-      <MobileNavBar/>
+    <div>
+      <StartPage onStart={handleStartPage} isStartOpen={isStartOpen} />
+      <div className="header_wrapper">
+        <NavBar onStart={handleStartPage} navList={navMenuList} logo={LOGO_BLACK}/>
+        <MobileNavBar/>
+      </div>
     </div>
   );
 }
-
--- src/views/components/NavBar.js
+++ src/views/components/NavBar.js
@@ -1,18 +1,19 @@
 import {Fragment} from "react";
 import {Link, useLocation} from "react-router-dom";
 
-export function NavBar ({variant = null, navList, logo,  onClick}) {
+export function NavBar ({variant = null, navList, logo,  onClick, onStart}) {
   const location = useLocation();
+
 
   return (
     <Fragment>
-      <div className={`logo_wrapper ${variant}`}>
-        <a href="#start">
+      <div className={`logo_wrapper ${variant}`} onClick={onStart}>
+        <button>
           <img alt="logo" src={logo}/>
-        </a>
+        </button>
       </div>
       <nav className={`nav_wrapper ${variant}`}>
-        <ul>
+        <ul className={variant ? "start" : ""}>
           {
             navList.map(({title, link}) => (
               <Fragment key={title}>
