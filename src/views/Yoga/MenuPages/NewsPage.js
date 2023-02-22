9dbbb57f319f7a57d7fab59fc78f5a308b34e0b4
max.kulbako <mkulbako@gmail.com>
2023-02-22 12:50:37 +0100

    added  scroll to sides of main page

--- src/views/Yoga/MenuPages/NewsPage.js
+++ src/views/Yoga/MenuPages/NewsPage.js
@@ -1,7 +1,38 @@
 export function NewsPage () {
   return (
-    <div>
-      News
+    <div className="news_page_container">
+      <div className="news_wrapper">
+        <p className="news_title">Новая модель организационной</p>
+        <p className="news_text">А ещё независимые государства описаны максимально подробно. Вот вам яркий пример современных тенденций — повышение уровня гражданского сознания играет определяющее значение для экспериментов, поражающих по своей масштабности и грандиозности.
+        </p>
+        <div className="news_img"
+        style={{
+          backgroundImage: `url("https://res.cloudinary.com/dd69ztxsd/image/upload/v1677049574/nana_yoga/Hatha_Yoga_odq6cv.jpg")`
+        }}
+        >
+          <svg width="109" height="109" viewBox="0 0 109 109" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="10" y="19" width="89" height="71" rx="13" stroke="currentColor" strokeWidth="2"/>
+            <path d="M67.5909 50.4985C70.346 52.0199 70.346 55.9801 67.5909 57.5015L48.4337 68.0811C45.7678 69.5533 42.5 67.625 42.5 64.5795L42.5 43.4205C42.5 40.375 45.7678 38.4467 48.4337 39.9189L67.5909 50.4985Z" stroke="currentColor" strokeWidth="2"/>
+          </svg>
+        </div>
+
+      </div>
+      <div className="news_wrapper">
+        <p className="news_title">Новая модель организационной</p>
+        <p className="news_text">А ещё независимые государства описаны максимально подробно. Вот вам яркий пример современных тенденций — повышение уровня гражданского сознания играет определяющее значение для экспериментов, поражающих по своей масштабности и грандиозности.
+        </p>
+        <div className="news_img"
+             style={{
+               backgroundImage: `url("https://res.cloudinary.com/dd69ztxsd/image/upload/v1677049574/nana_yoga/Hatha_Yoga_odq6cv.jpg")`
+             }}
+        >
+          <svg width="109" height="109" viewBox="0 0 109 109" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="10" y="19" width="89" height="71" rx="13" stroke="currentColor" strokeWidth="2"/>
+            <path d="M67.5909 50.4985C70.346 52.0199 70.346 55.9801 67.5909 57.5015L48.4337 68.0811C45.7678 69.5533 42.5 67.625 42.5 64.5795L42.5 43.4205C42.5 40.375 45.7678 38.4467 48.4337 39.9189L67.5909 50.4985Z" stroke="currentColor" strokeWidth="2"/>
+          </svg>
+        </div>
+
+      </div>
     </div>
   );
 }
