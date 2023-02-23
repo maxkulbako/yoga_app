20605340ce37851c72fa9ca02a3d7c0bcc33a322
max.kulbako <mkulbako@gmail.com>
2023-02-23 16:48:01 +0100

    add scroll hook and change geader behavior

--- src/views/components/Header.js
+++ src/views/components/Header.js
@@ -3,10 +3,12 @@ import { MobileNavBar } from "./MobileNavBar";
 import { LOGO_BLACK, navMenuList } from "../../core/constants/storage";
 import { StartPage } from "./StartPage";
 import React, {useState} from "react";
+import { useScrollDirection } from "../../core/hooks";
 
 
 export function Header () {
   const [isStartOpen, setOpenStart] = useState(true);
+  const scrollDirection = useScrollDirection();
 
   const handleStartPage = () => {
     setOpenStart(!isStartOpen);
@@ -15,7 +17,7 @@ export function Header () {
   return (
     <div className="header_container">
       <StartPage onStart={handleStartPage} isStartOpen={isStartOpen} />
-      <div className="header_wrapper">
+      <div className={`header_wrapper ${ scrollDirection === "down" ? "hide" : "show" }`}>
         <NavBar onStart={handleStartPage} navList={navMenuList} logo={LOGO_BLACK}/>
         <MobileNavBar/>
       </div>
