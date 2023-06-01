e666c81e7edc59df6e274e4d90abfdc97d75e1b5
max.kulbako <mkulbako@gmail.com>
2023-06-01 10:47:26 +0200

    add offline page

--- src/views/components/Buttons/BaseButton.js
+++ src/views/components/Buttons/BaseButton.js
@@ -1,10 +1,11 @@
 import "./base_button.scss";
 
-export function BaseButton({ title, variant, onClick, type = "button" }) {
+export function BaseButton({ title, variant, onClick, type = "button", icon }) {
   return (
     <div className={`base_btn_wrapper ${variant || ""}`}>
       <button onClick={onClick} type={type} className="base_btn">
         <p>{title}</p>
+        {icon}
       </button>
     </div>
   );
